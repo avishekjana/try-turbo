60afde037e0b07effd101e938ee080aafc7d3fc7
Avishek Jana <avishek@geogo.in>
2022-07-21 17:45:42 +0530

    Turbo stream implemented

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,9 @@
 class Quote < ApplicationRecord
   validates :name, presence: true
+
+  # after_create_commit -> { broadcast_prepend_to "quotes" }
+  # after_update_commit -> { broadcast_replace_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
